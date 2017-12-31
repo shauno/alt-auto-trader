b2f057d1167e3b43a0f369e791e1c02a54152c54
Shaun Alberts <shaunalberts@gmail.com>
2017-12-31 21:39:03 +0200

    Add exchanges table so we can differentiate different APIs

---
+++ database/migrations/2017_12_31_193322_add_exchange_id_to_exchange_rates.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExchangeIdToExchangeRates extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('exchange_rates', function (Blueprint $table) {
+            $table->integer('exchange_id')->unsigned()->after('id');
+            $table->foreign('exchange_id')->references('id')->on('exchanges');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('exchange_rates', function (Blueprint $table) {
+            $table->dropColumn('exchange_id');
+        });
+    }
+}
